3f0b90cc5f858a0ea3fd9be7a153d35c649a2427
Samuel_Avila <samuel.avila@tekever.com>
2025-03-13 21:04:31 +0000

    creating ecr stack

--- bin/e_commerce_ecscdk.ts
+++ bin/e_commerce_ecscdk.ts
@@ -1,20 +1,18 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { ECommerceEcscdkStack } from '../lib/e_commerce_ecscdk-stack';
+import { EcrStack } from '../lib/ecr-stack';
 
 const app = new cdk.App();
-new ECommerceEcscdkStack(app, 'ECommerceEcscdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const env: cdk.Environment = {
+  account: "654654185962",
+  region: "us-east-1"
+}
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const tagsInfra = { // colar um adesivo de acordo com o custo de operação 
+  cost: "ECommerceInfra",
+  team: "SamuCode"
+}
+const ecrStack = new EcrStack(app, "Ecr", {
+  env: env,
+  tags: tagsInfra
 });
